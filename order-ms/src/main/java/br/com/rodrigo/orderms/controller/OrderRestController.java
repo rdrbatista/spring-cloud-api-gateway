c360acdd12cdd3c9017a7625b602cf5645f28c01
RODRIGO BATISTA RAMOS SILVA <4037289@riachuelo.com.br>
2022-05-04 08:47:30 -0300

    first commit

---
+++ order-ms/src/main/java/br/com/rodrigo/orderms/controller/OrderRestController.java
@@ -0,0 +1,19 @@
+package br.com.rodrigo.orderms.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderRestController {
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/info")
+    public ResponseEntity<String> showOrderInfo() {
+        return ResponseEntity.ok("FROM ORDER SERVICE, Port# is: " + port);
+    }
+}
